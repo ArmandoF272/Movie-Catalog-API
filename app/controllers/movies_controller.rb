cced88f1f2828caa38269f67a08ed207d6e4dfc5
Armando Vieira <programador12@speedgov.com.br>
2024-10-25 17:23:52 -0300

    Feat: adding method to convert the object to json

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,3 +1,14 @@
 class MoviesController < ApplicationController
-
+  private
+    def movie_json(movie)
+      {
+        id: movie.id.to_s,
+        title: movie.title,
+        genre: movie.genre,
+        year: movie.year,
+        country: movie.country,
+        published_at: movie.published_at,
+        description: movie.description
+      }
+    end
 end
