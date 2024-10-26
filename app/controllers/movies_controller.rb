f217ae60c908fa27229a4ad908b0d9f84dadeed7
Armando Vieira <programador12@speedgov.com.br>
2024-10-26 15:49:14 -0300

    Fix: adapting methods that perform filters and calls the genre attribute

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,7 @@ class MoviesController < ApplicationController
       {
         id: movie.id.to_s,
         title: movie.title,
-        genre: movie.genre,
+        genre: movie.genre.join(", "),
         year: movie.year,
         country: movie.country,
         published_at: movie.published_at,
@@ -21,7 +21,7 @@ class MoviesController < ApplicationController
 
     def filter_movies(movies, params)
       movies = movies.where(year: params[:year]) if params[:year].present?
-      movies = movies.where(genre: params[:genre]) if params[:genre].present?
+      movies = movies.where(:genre.in => [params[:genre]]) if params[:genre].present?
       movies = movies.where(country: params[:country]) if params[:country].present?
       movies = movies.where(published_at: params[:published_at]) if params[:published_at].present?
       movies = movies.where(description: params[:description]) if params[:description].present?
