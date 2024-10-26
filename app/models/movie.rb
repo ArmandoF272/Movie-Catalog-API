5a7f6d0378f1d22a9df567c1b973a48966bdf41c
Armando Vieira <programador12@speedgov.com.br>
2024-10-26 15:43:02 -0300

    Feat: Adding new attributes to the Movies model and adjusting checks

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,13 +2,21 @@ class Movie
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  field :show_id, type: String
   field :title, type: String
-  field :genre, type: String
+  field :genre, type: Array
   field :year, type: Integer
   field :country, type: String
   field :published_at, type: Date
   field :description, type: String
+  field :type, type: String
+  field :director, type: String
+  field :cast, type: String
+  field :release_year, type: Integer
+  field :rating, type: String
+  field :duration, type: String
+  field :listed_in, type: String
 
-  validates :title, presence: true, uniqueness: { case_sensitive: false }
-  validates :year, :genre, :country, :published_at, :description, presence: true
+  validates :show_id, presence: true, uniqueness: { case_sensitive: false }
+  validates :title, :description, presence: true
 end
